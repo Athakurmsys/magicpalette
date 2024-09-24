02258b69be77921407cd3c0e36776dcd01855423
Athakurmsys <athakur@msystechnologies.com>
2024-09-24 18:11:44 +0530

    minor fixes

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -8,10 +8,10 @@ export interface MGPButtonProps extends MUIButtonProps {
     label?: string;
 }
 
-'text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800'
+
 
 const Button = forwardRef<ButtonProps, MGPButtonProps>(({label, className, children, ...props}, ref) =>{
-    return  <MUIButton {...props} className={cn(className)}>{children? children : label}</MUIButton>
+    return  <MUIButton {...props} className={cn('text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800',className)}>{children? children : label}</MUIButton>
 })
 
 
